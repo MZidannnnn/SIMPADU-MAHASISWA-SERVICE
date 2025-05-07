d4afd343fc3e46bb9474525d244f67734524312d
Ilham <rilehammer@gmail.com>
2025-05-07 14:41:10 +0800

    mengubah dan menambah codingan di migration dan model di kol_jurusan_sekolah

--- database/migrations/2025_05_06_045940_create_kol_jurusan_sekolah_table.php
+++ database/migrations/2025_05_06_045940_create_kol_jurusan_sekolah_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('kol_jurusan_sekolah', function (Blueprint $table) {
-            $table->string('id_jurusan_sekolah', 50)->primary()->collation('latin1_general_ci');
-            $table->string('nama_sekolah', 50)->collation('latin1_general_ci');
+            $table->char('id_jurusan_sekolah', 50)->primary()->collation('latin1_general_ci');
+            $table->string('nama', 50)->collation('latin1_general_ci');
+            $table->string('nama_jurusan', 100)->collation('latin1_general_ci');
             $table->timestamps();
         });
     }
