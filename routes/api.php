4fde973a33b3440282300ca5b993e3f672fdd3e3
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2025-05-28 14:46:10 +0800

    feat : menambahkan dokumentasi API menggunakan Swagger Editor (OpenAPI 3.0)

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\PengumumanController;
 
 Route::get('/tes', fn () => 'API Tes OK!');
 
+Route::get('/docs/openapi', function () {
+    return response()->file(public_path('docs/openapi.yaml'));
+});
+
 Route::get('/mahasiswa/list_mahasiswa', [MahasiswaController::class, 'listMahasiswa']);
 Route::get('/mahasiswa/orangtua/nim/{nim}', [OrangTuaController::class, 'showByNim']);
 
