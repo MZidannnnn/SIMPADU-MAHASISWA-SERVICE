06863fb310b39951606e7728a8698aa5279cadb7
Muhammad Zidan Helmy <mzidanhelmyy@gmail.com>
2025-06-24 17:56:47 +0800

    feat: penambahan konfigurasi CORS, middleware auth & perbaikan dokumentasi API
    Perubahan yang dilakukan:
    - Menambah konfigurasi CORS untuk mengizinkan akses dari frontend lokal dan produksi.
    - Menambah middleware 'VerifyAuthToken' untuk integrasi dengan service auth. (Middleware belum diimplementasikan pada route karena menunggu API verifikasi token dari service auth).
    - Memperbaiki dan melengkapi dokumentasi API pada Swagger.

---
+++ config/cors.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://ti054d04.agussbn.my.id',
+        'https://ti054d02.agussbn.my.id',
+        'https://ti054d01.agussbn.my.id',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
+];
--- routes/api.php
+++ routes/api.php
@@ -6,22 +6,15 @@ use App\Http\Controllers\OrangTuaController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PengumumanController;
 
-// tes api
-Route::get('/tes', fn () => 'API Tes OK!');
-
-// CRUD orangtua,store,put,show,delete,get
-Route::apiResource('/mahasiswa/orangtua', OrangTuaController::class);
-
-// CRUD pengumuman,store,put,show,delete,get
-Route::apiResource('pengumuman', PengumumanController::class);
-
-
-// Route::apiResource('mahasiswa', MahasiswaController::class)->parameters([
-    //      'mahasiswa' => 'nim',
-    // ]);
+// Route::middleware('has.token')->group(function () {
+    // });
     
 // tampilkan nim,dan nama seluru mahasiswa
 Route::get('/mahasiswa/list_mahasiswa', [MahasiswaController::class, 'listMahasiswa']);
+// CRUD orangtua,store,put,show,delete,get
+Route::apiResource('/mahasiswa/orangtua', OrangTuaController::class);
+// CRUD pengumuman,store,put,show,delete,get
+Route::apiResource('pengumuman', PengumumanController::class);
 // Tampilkan detail mahasiswa
 Route::get('/mahasiswa/{nim}', [MahasiswaController::class, 'show']);
 // Tambah mahasiswa
@@ -31,6 +24,4 @@ Route::put('/mahasiswa/{nim}', [MahasiswaController::class, 'update']);
 Route::patch('/mahasiswa/{nim}', [MahasiswaController::class, 'update']);
 // Hapus mahasiswa
 Route::delete('/mahasiswa/{nim}', [MahasiswaController::class, 'destroy']);
-
 Route::get('/status_mhs', [MahasiswaController::class, 'DaftarStatusMahasiswa']);
-
